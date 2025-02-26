b1a40273b6e5f6d7177ceed7fac02e029b7dd1c1
Frédéric Blanc <fblanc@groupeastek.fr>
2025-02-26 19:05:25 +0100

    Register new facades

--- src/OpenFoodFactsServiceProvider.php
+++ src/OpenFoodFactsServiceProvider.php
@@ -23,8 +23,19 @@ class OpenFoodFactsServiceProvider extends ServiceProvider
     {
         $this->mergeConfigFrom(__DIR__.'/../config/openfoodfacts.php', 'openfoodfacts');
 
+        // Binding for "beauty" environment
         $this->app->singleton('openfoodfacts', function (Container $app) {
             return new OpenFoodFacts($app);
+        });
+
+        // Binding for "beauty" environment
+        $this->app->singleton('openbeautyfacts', function ($app) {
+            return new OpenFoodFacts($app, environment:'beauty');
+        });
+
+        // Binding for "pet" environment
+        $this->app->singleton('openpetfoodfacts', function ($app) {
+            return new OpenFoodFacts($app, environment:'pet');
         });
     }
 }
