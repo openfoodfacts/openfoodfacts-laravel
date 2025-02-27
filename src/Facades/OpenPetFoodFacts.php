e3d9920f72f742434a057fc0d2a21221005e9574
Eddie Palmans <eddie@palmans.nl>
2025-02-27 09:24:00 +0100

    php cs fixer

--- src/Facades/OpenPetFoodFacts.php
+++ src/Facades/OpenPetFoodFacts.php
@@ -17,7 +17,7 @@ class OpenPetFoodFacts extends Facade
      * Get the registered name of the component.
      *
      * @return string
-     */    
+     */
     protected static function getFacadeAccessor()
     {
         return 'openpetfoodfacts';
